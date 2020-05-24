7a30b499753c3846cec89a51df09db5b09d0a9a5
MacBook <alinauroze@hotmail.com>
2020-05-24 12:15:48 +0500

    helper: generate key

--- models/Lobby.js
+++ models/Lobby.js
@@ -8,3 +8,21 @@ class Lobby {
 		this.max = max;
 	}
 }
+
+/*
+	Helper functions are used by Lobby Class
+*/
+
+const helper = {
+	generateKey(size = 6) {
+		let key = "";
+		let chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+		for (i = 0; i < size; i++) {
+			let i_ = Math.floor(Math.random() * 36);
+			key += chars[i_];
+		}
+		return key;
+	}
+}
+
+module.exports = Lobby;
