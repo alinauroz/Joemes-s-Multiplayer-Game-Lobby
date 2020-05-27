bbdd5016f3f4b3633bc2f98a89c2f958f0947f68
MacBook <alinauroze@hotmail.com>
2020-05-27 23:22:08 +0500

    register online user

--- io/io.js
+++ io/io.js
@@ -1,7 +1,32 @@
+const users = new Object();
+
+const err = {
+    "noid" : "Id must be provided when join",
+    "already_present" : "You are already logged in from another window. Please close all other tabs or windows.",
+
+}
+
 module.exports = function (server) {
     const io = require("socket.io")(server);
 
     io.on("connection", socket => {
-        console.log("User connected");
-    })
+
+        socket.on("register", data => {
+            if (data.id) {
+                if (data.id in users) {
+                    socket.emit("register", {err : err['already_present']})
+                }
+                else {
+                    users[data.id] = new Object();
+                    users[data.id].socket = socket;
+                }
+            }
+            else {
+                socket.emit("register", {err : err['noid']});
+            }
+        });
+
+        
+
+    });
 }
